74dddb8be5addc2cb54832c914f1599755b71abb
Diego Pagini <diegopagini777@gmail.com>
2020-10-23 14:58:20 -0300

    Js terminado

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// API 
-/*
 const APIkey = "9eec655c6fd34430a775dd9fd71dd384"
 
 function getApi(CP) {
@@ -8,28 +6,79 @@ function getApi(CP) {
   .then((data) => {
     currentData = data.data
     console.log(currentData)
+    showDate(0, 'cp')
+    showTemp(0, 'temp')
+    showRH(0, 'rh')
+    showClouds(0, 'clouds')
+    showRain(0, 'rain')
+    showMaxTemp(0, 'max')
+    showMinTemp(0, 'min')
+    showDate(1,'dateFirst')
+    showTemp(1, 'tempFirst')
+    showDate(2, 'dateSecond')
+    showTemp(2, 'tempSecond')
+    showDate(3, 'dateThird')
+    showTemp(3, 'tempThird')
+    showDate(4, 'dateFourth')
+    showTemp(4, 'tempFourth')
+    showDate(5, 'dateFifth')
+    showTemp(5, 'tempFifth')
   })
 }
 getApi(7600)
-// Fuera de esta funcion, currenData no esta disponible. Como lo saco?
 
-
-// ¿Por que no anda? currentData es un array de objetos.
-function B(){
-  currentData.forEach(element => {
-    console.log(element)
-  });
+//Mostrar fecha actual
+function showDate(i, id){
+  cp = `
+    <h2>${currentData[i].datetime}</h2>
+  `
+  document.getElementById(id).innerHTML = cp;
 }
-B()
 
-// Por que esto no se ejecuta??
-function A(){
-  for(i=0;i<currentData.length;i++){
-    console.log(currentData[i])
-  }
+//Mostrar temperatura actual
+function showTemp(i, id){
+  temp = `
+  <h1 class="right">${currentData[i].temp} °</h1>
+  `
+  document.getElementById(id).innerHTML = temp;
 }
-A()
 
-// Estp tampoco anda, empiezo a pensar que tengo un problema con currentData
-console.log(currentData[0])
-*/
+//Mostrar la humedad actual
+function showRH(i, id){
+  rh = `
+  ${currentData[i].rh} % de humedad
+  `
+  document.getElementById(id).innerHTML = rh;
+}
+
+//Mostrar la nobosidad actual
+function showClouds(i, id){
+  clouds = `
+  ${currentData[i].clouds} % de nubosidad
+  `
+  document.getElementById(id).innerHTML = clouds;
+}
+
+//Mostrar la probabilidad de lluvia actual
+function showRain(i, id){
+  rain = `
+  ${currentData[i].precip} % de que llueva
+  `
+  document.getElementById(id).innerHTML = rain;
+}
+
+//Mostrar la temperatura maxima
+function showMaxTemp(i, id){
+  max = `
+  ${currentData[i].app_max_temp} ° de máxima
+  `
+  document.getElementById(id).innerHTML = max;
+}
+
+//Mostrar la temperatura minima
+function showMinTemp(i, id){
+  min = `
+  ${currentData[i].app_min_temp} ° de minima
+  `
+  document.getElementById(id).innerHTML = min;
+}
